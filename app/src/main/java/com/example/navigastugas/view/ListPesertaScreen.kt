97fe538da20d95803dff1ccdc04fe5d236b7342d
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2025-11-08 00:15:16 +0700

    menambahkan composable ListPesertaScreen dengan parameter goToForm dan goToHome

---
+++ app/src/main/java/com/example/navigastugas/view/ListPesertaScreen.kt
@@ -0,0 +1,20 @@
+package com.example.navigastugas.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.BorderStroke
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ListPesertaScreen(
+    goToForm: () -> Unit,
+    goToHome: () -> Unit = {}
+) {}
